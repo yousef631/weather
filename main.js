98f57607e4c763bd4a06823cd5a2a12ca716b192
yousef631 <115421669+yousef631@users.noreply.github.com>
2023-08-22 23:16:56 +0300

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var temp= document.getElementById("h");
 async function checkweather(country){
     var check = await fetch("https://api.openweathermap.org/data/2.5/weather?q="+country+"&appid=0fb824da6a2bcff1e819cb6668f370e2&units=metric");
     var data = await check.json();
-    temp.innerHTML=data.main.temp +'c°'
+    temp.innerHTML=Math.round(data.main.temp) +'c°'
 }
 btn.onclick =function(){
     checkweather(input.value)
